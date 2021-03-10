21c9b55642002117e98f9a2279acbb9343bb11e1
Бажен Левковець <bazhen.paseka@gmail.com>
2021-03-10 21:39:12 +0200

    Manipulation of handlers.
    Manipulation pf handlers 2
    
    
    .
    
    
    .

--- VRbox-keyboards-sm.h
+++ VRbox-keyboards-sm.h
@@ -28,6 +28,7 @@
 	#include "VR-box-keyboards-local-config.h"
 	#include "keyb_4x4_pcf8574_i2c_sm.h"
 
+
 /*
 **************************************************************************
 *								    DEFINES                     
@@ -42,10 +43,24 @@
 
 /*
 **************************************************************************
+*								   	ENUM
+**************************************************************************
+*/
+	typedef enum
+	{
+		VR_KeyBoard_0	,
+		VR_KeyBoard_1	,
+		VR_KeyBoard_QNT	,
+	} VR_Keyboard_enum ;
+/*
+**************************************************************************
 *								GLOBAL VARIABLES
 **************************************************************************
 */
-
+	typedef struct {
+		I2C_HandleTypeDef  	i2c		;
+		UART_HandleTypeDef 	uart	;
+	} 		Debug_Struct			;
 /*
 **************************************************************************
 *									 MACRO'S                     
@@ -57,8 +72,17 @@
 *                              FUNCTION PROTOTYPES
 **************************************************************************
 */
-	void VRbox_keyboards_Init (void) ;
-	void VRbox_keyboards_Main (void) ;
+	void VRbox_Debug_Init(	Debug_Struct	 	*_debug	,
+							UART_HandleTypeDef	*_uart ,
+							I2C_HandleTypeDef	*_i2c	) ;
+
+	void VRbox_Main(		Debug_Struct 		*_debug ,
+							PCF8574_Struct  	*_pcf0	,
+							PCF8574_Struct  	*_pcf1	) ;
+
+	void Debug_print_key(	Debug_Struct 		*_debug 	,
+							PCF8574_Struct		*_pcf		,
+							uint8_t 			_button_u8	) ;
 /*
 **************************************************************************
 *                              		  END
